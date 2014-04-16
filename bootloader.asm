468c0f231a10676d52fddce3fabee6add87a8719
Joe Savage <joe@dev-hq.net>
2014-04-16 11:10:13 +0100

    fix indentation

--- bootloader.asm
+++ bootloader.asm
@@ -39,4 +39,4 @@ print:
 
 ; Pad to 510 bytes (boot sector size minus 2) with 0s, and finish with the two-byte standard boot signature
 times 510-($-$$) db 0 
-dw 0xAA55           ; => 0x55 0xAA (little endian byte order)
+dw 0xAA55	        ; => 0x55 0xAA (little endian byte order)
