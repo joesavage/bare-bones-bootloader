d5f1668acd133865065816bff98097182e497f88
Felix Dietrich <felix.dietrich@sperrhaken.name>
2015-03-29 00:57:25 +0100

    Rename bootloader.asm --> bootloader.nasm

--- diskimage.sh
+++ diskimage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-nasm -f bin -o bootloader.bin bootloader.asm
+nasm -f bin -o bootloader.bin bootloader.nasm
 dd conv=notrunc bs=512 count=1 if=bootloader.bin of=bootloader.flp
 rm bootloader.bin
